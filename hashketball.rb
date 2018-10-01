6019dbe98d585795825de41356ae6d71b591783f
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-10-01 13:25:47 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -152,6 +152,7 @@ def team_names
 end
 
 def player_numbers(team_name)
+    
     game_hash.each do |location, team_data|
       if team_data[:team_name] === team_name
         team_data[:players].each do |player, number|
